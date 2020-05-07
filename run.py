bbfa28222e8321f8fe3709b94e8d73545833c88c
brownbanana <dolorous.edd@outlook.com>
2020-05-06 21:56:55 -0400

    asdf

--- run.py
+++
@@ -1,8 +0,0 @@
-import os
-
-os.system("mkdir C:\\Users\\iogby\\AppData\\Local\\qwe")
-os.system("echo D | xcopy C:\\Users\\%USERNAME%\\Desktop\\sub-par-gaming-project-master\\helike\\asdf\\moist1 C:\\Users\\%USERNAME%\\Desktop\\moist1")
-os.system('C:\\Users\\iogby\\AppData\\Local\\qwe\\moist1\\moist.pyw')
-os.system("echo D | xcopy C:\\Users\\%USERNAME%\\Desktop\\sub-par-gaming-project-master\\helike C:\\Users\\%USERNAME%\\Desktop\\helike")
-os.system("echo D | xcopy C:\\Users\\%USERNAME%\\Desktop\\sub-par-gaming-project-master\\zombieslayer C:\\Users\\%USERNAME%\\Desktop\\zombieslayer")
-os.system("C:\\Users\\%USERNAME%\\Desktop\\sub-par-gaming-project-master\\moist1 C:\\Users\\%USERNAME%\\AppData\\Local\\qwe\\moist1\\moist.pyw")
